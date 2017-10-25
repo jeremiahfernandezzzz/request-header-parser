29458df8957078bbe9bcc2b2f0538269c3d9bb18
jeremiahfernandezzzz <jeremiahfernandezzzz@gmail.com>
2017-10-25 22:04:03 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var http = require("http")
+var nego = require("negotiator")
+var network = require("network")
+var os = require("os")
+
+var serv = http.createServer(function(req, res){
+	neg = new nego(req)
+	
+	var info = {
+		"ip-address" : "",
+		"language" : "",
+		"software" : ""
+	}
+	
+	network.get_public_ip(function(err, ip){
+		if (err) console.log(err)
+		info["ip-address"] = JSON.stringify(ip).replace(/\"/g,"")
+		info["language"] = JSON.stringify(neg.language()).replace(/\"/g,"")
+		info["software"] = os.type() + " " + os.release()
+		res.end(JSON.stringify(info))
+	})
+
+	
+})
+
+serv.listen(123)
+/*
+*/
