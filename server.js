79c98f3def80ec338fe64f1f8f32dcb47e04c36e
jopet <jeremiahfernandezzzz@gmail.com>
2017-10-26 10:02:44 +0800

    fixed dependencies

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 var http = require("http")
-var nego = require("negotiator")
-var network = require("network")
-var os = require("os")
 
 var serv = http.createServer(function(req, res){
-	neg = new nego(req)
 	
+	//var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+  //var language = req.headers["accept-language"].split(',')[0];
+  //var software = req.headers["user-agent"].split(') ')[0].split(' (')[1]
+  
 	var info = {
-		"ip-address" : "",
-		"language" : "",
-		"software" : ""
+		"ip-address" : req.headers['x-forwarded-for'].split(',')[0] || req.connection.remoteAddress.split(',')[0] || req.socket.remoteAddress.split(',')[0] || req.connection.socket.remoteAddress.split(',')[0],
+		"language" : req.headers["accept-language"].split(',')[0],
+		"software" : req.headers["user-agent"].split(') ')[0].split(' (')[1]
 	}
-	
-	network.get_public_ip(function(err, ip){
+		res.end(JSON.stringify(info))
+  
+	//var neg = new nego(req)
+	/*
+	network.get_gateway_ip(function(err, ip){
 		if (err) console.log(err)
 		info["ip-address"] = JSON.stringify(ip).replace(/\"/g,"")
 		info["language"] = JSON.stringify(neg.language()).replace(/\"/g,"")
 		info["software"] = os.type() + " " + os.release()
 		res.end(JSON.stringify(info))
 	})
-
-	
+  */
 })
 
-serv.listen(process.env.PORT || 3000)
+serv.listen(process.env.PORT || 8080)
 /*
 */
